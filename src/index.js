930ff72fb351ca047c2043a2474149504b1d8d71
Ruby Rinken <rubyrinken@gmail.com>
2020-04-16 14:42:51 -0600

    Add user page, styling

--- src/index.js
+++ src/index.js
@@ -6,10 +6,38 @@ import LoginManager from './login-manager';
 
 import './css/base.scss';
 
+let user;
+
 $("#login-submit").click(() => {
   const username = $("#username").val();
   const pass = $("#password").val();
-  const login = new LoginManager(username, pass, function() {
-    console.log(login.userData);
-  });
+  login(username, pass);
 });
+
+function login(username, pass) {
+  const login = new LoginManager(username, pass, function() {
+    user = login.userData;
+    if (login.id === "manager") {
+      loadManagerPage();
+    } else {
+      loadUserPage();
+    }
+    hideLoginPage();
+  });
+}
+
+function hideLoginPage() {
+  $("#login-page").slideUp(1000);
+}
+
+function loadManagerPage() {
+
+}
+
+function loadUserPage() {
+  $("#user-page").slideDown(1000);
+  $("#user-name").append(user.name + "!");
+  $("#user-bookings").html(`<p>You have x upcoming bookings.</p>`);
+}
+
+// login("customer23", "overlook2019");
