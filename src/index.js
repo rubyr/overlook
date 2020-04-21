5de71f60d56a399583aa64cc4b54a32adb979c37
Ruby Rinken <rubyrinken@gmail.com>
2020-04-21 13:29:02 -0600

    Allow managers to see, remove, and add bookings for users

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ let user;
 let roomRepo = new RoomRepo();
 let loginManager = new LoginManager();
 let today = getToday();
+const bookingDate = datepicker("#booking-calendar", {
+  minDate: new Date(),
+  onSelect: showOpenRooms
+});
 
 function getToday() {
   return moment().format("YYYY/MM/DD");
@@ -39,6 +43,10 @@ function login() {
     id: "date",
     text: today
   }).appendTo("header");
+
+  $("aside").delay(1000).show();
+
+  $("#new-booking").show();
 
   if (loginManager.id === "manager") {
     loadManagerPage();
@@ -78,6 +86,58 @@ function loadManagerPage() {
     }%</span> of rooms are booked.`
   }).appendTo("#percent-occupied");
 
+  $("aside").prepend(
+    `<label for="manager-customer-search">Customer Search</label>
+    <input id="manager-customer-search"></input>
+    <section id="manager-customer-stats" style="display:none"></section>`
+  );
+
+  $("#manager-customer-search").on('keyup', (event) => {
+    const query = $("#manager-customer-search").val().toLowerCase();
+    if (event.keyCode === 13) {
+      const userData = user.find(
+        (usr) => usr.name.toLowerCase().includes(query)
+      );
+
+      if (userData !== undefined) {
+        initForm(userData.id);
+        const bookings = roomRepo.filterBookingsByUser(userData.id);
+        $("#manager-customer-stats").html(
+          `<h3>${userData.name}</h3>
+          <ul>` + bookings.map(book => {
+            const future = compareDates(book.date, today) === 1;
+            return `<li data-booking-id=${book.id}>
+              ${future ? `<button class="remove-booking">&times;</button>` : ""}
+              Room ${book.roomNumber} on ${book.date}
+            </li>`
+          }).join("") + `</ul>`
+        ).show();
+
+        $("button.remove-booking").click(function() {
+          const url = "https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings";
+          const id = parseInt(this.closest("li").dataset.bookingId);
+          fetch(url, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id})
+          }).then(response => {
+            if (response.ok) {
+              this.closest("ul").removeChild(this.closest("li"));
+            } else {
+              console.log(response);
+            }
+          }).catch(err => console.log(err));
+        });
+      } else {
+        $("#manager-customer-stats").html(
+          `Sorry, but there were no users matching that name.`
+        ).show();
+      }
+    }
+  });
+
   $("#manager-page").slideDown(1000);
 }
 
@@ -85,44 +145,37 @@ function loadUserPage() {
   $("#user-page").slideDown(1000);
   $("#user-name").append(user.name + "!");
   const bookings = roomRepo.filterBookingsByUser(user.id);
+  const pastBookings = bookings.filter(b => compareDates(b.date, today) === -1);
+  const futureBookings = bookings.filter(b => compareDates(b.date, today) === 1);
   $("#user-bookings").html(
-    `<h3>Previous bookings:</h3><table class="date-table">${bookings.map(booking => 
-      `<tr><td>${booking.date}</td><td>Room ${booking.roomNumber}</td></tr>`).join("")
+    `<h3>Previous bookings:</h3><table class="date-table">${
+      pastBookings.map(booking => `<tr><td>${booking.date}</td><td>Room ${
+        booking.roomNumber}</td></tr>`).join("")
+    }</table>`
+  );
+
+  $("#user-upcoming-bookings").html(
+    `<h3>Upcoming bookings:</h3><table class="date-table">${
+      futureBookings.map(booking => `<tr><td>${booking.date}</td><td>Room ${
+        booking.roomNumber}</td></tr>`).join("")
     }</table>`
   );
   $("#user-spent").html(
     `You have spent <span class="bigtext">$${roomRepo.getTotalBookingsPrice(bookings)}</span> on rooms.`
   );
 
-  const bookingDate = datepicker("#booking-calendar", {
-    minDate: new Date(),
-    onSelect: (instance, date) => {
-      date = moment(date).format("YYYY/MM/DD");
-      const rooms = roomRepo.getOpenRooms(date);
-      if (rooms.length === 0) {
-        $("#booking-form").hide();
-        $("#apology").show();
-      } else {
-        $("#apology").hide();
-        $("#booking-form").show();
-        $("#booking-open-rooms").html(
-          rooms.map(room => 
-            `<option value="${room.number}" data-room-type="${room.roomType}">Room ${room.number} (${
-              room.numBeds} ${room.bedSize
-            }-size beds, $${room.costPerNight}/ night)</option>`
-          ).join("")
-        );
-      }
-    } 
-  });
+  initForm();
+}
 
+function initForm(userId) {
   $("#booking-filter-room-type").click(() => {
     $("#booking-open-rooms").children().hide();
     var selected = $("#booking-filter-room-type")
       .children("input:checked")
-      .map(function() { return this.value })
-      .get();
-    console.log(selected);
+      .map(function() { 
+        return this.value 
+      }).get();
+    
     selected.forEach(opt => {
       opt = opt.replace(/_/g, " ");
       $("#booking-open-rooms").children().filter(`option[data-room-type="${opt}"]`).show();
@@ -131,7 +184,7 @@ function loadUserPage() {
 
   $("#booking-submit").click(() => {
     const url = "https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings";
-    const userID = user.id;
+    const userID = user.id || userId;
     const date = moment(bookingDate.dateSelected).format("YYYY/MM/DD");
     const roomNumber = parseInt($("#booking-open-rooms").val());
     fetch(url, {
@@ -140,7 +193,47 @@ function loadUserPage() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({userID, date, roomNumber})
-    }).then(response => console.log(response))
-      .catch(err => console.log(err));
+    }).then(response => {
+      if (response.ok) {
+        $("#booking-calendar").val("");
+        $("#booking-form").hide();
+        $("#booking-status").html(
+          `Congratulations! You're booked for Room ${roomNumber} on ${date}`
+        ).addClass("status-success").removeClass("status-error")
+          .delay(5000).slideUp(1000);
+      } else {
+        $("#booking-status").html(
+          "Sorry, something went wrong. Please try again."
+        ).addClass("status-error").removeClass("status-success")
+          .delay(5000).slideUp(1000);
+      }
+    }).catch(err => console.log(err));
   });
+}
+
+function compareDates(a, b) {
+  a = a.split("/").map(a => Number(a));
+  b = b.split("/").map(b => Number(b));
+  a = new Date(a[0], a[1] - 1, a[2]);
+  b = new Date(b[0], b[1] - 1, b[2]);
+  return a > b ? 1 : -1;
+}
+
+function showOpenRooms(datepicker, date) {
+  date = moment(date).format("YYYY/MM/DD");
+  const rooms = roomRepo.getOpenRooms(date);
+  if (rooms.length === 0) {
+    $("#booking-form").hide();
+    $("#apology").show();
+  } else {
+    $("#apology").hide();
+    $("#booking-form").show();
+    $("#booking-open-rooms").html(
+      rooms.map(room => 
+        `<option value="${room.number}" data-room-type="${room.roomType}">Room ${room.number} (${
+          room.numBeds} ${room.bedSize
+        }-size beds, $${room.costPerNight}/ night)</option>`
+      ).join("")
+    );
+  }
 }
